7a19783fff7799f4763b5fbb9003642d796f22e8
watcharaphans <watcharaphans@gmail.com>
2018-08-17 11:01:21 +0700

    Add Fragment

--- app/src/main/java/watcharaphans/bitcombine/co/th/bitcamera/MainActivity.java
+++ app/src/main/java/watcharaphans/bitcombine/co/th/bitcamera/MainActivity.java
@@ -3,14 +3,26 @@ package watcharaphans.bitcombine.co.th.bitcamera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import watcharaphans.bitcombine.co.th.bitcamera.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
-
-
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        Add Fragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.contentFragmentMain, new MainFragment())
+                    .commit();
+
+        }
+
+
+    }  // Main Method
+
+}  // Main class
---
+++ app/src/main/java/watcharaphans/bitcombine/co/th/bitcamera/fragment/MainFragment.java
@@ -0,0 +1,22 @@
+package watcharaphans.bitcombine.co.th.bitcamera.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import watcharaphans.bitcombine.co.th.bitcamera.R;
+
+public class MainFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+} //Main Class
