c88683ba2e9d41d6ecdb2e0c14648c19cf4de3a8
watcharaphans <watcharaphans@gmail.com>
2018-08-17 11:37:47 +0700

    QR OK

--- app/src/main/java/watcharaphans/bitcombine/co/th/bitcamera/fragment/MainFragment.java
+++ app/src/main/java/watcharaphans/bitcombine/co/th/bitcamera/fragment/MainFragment.java
@@ -7,9 +7,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
 import watcharaphans.bitcombine.co.th.bitcamera.R;
 
 public class MainFragment extends Fragment{
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create QRscanner
+        ZXingScannerView zXingScannerView = getView().findViewById(R.id.zxingMain);
+        zXingScannerView.startCamera();
+
+
+    } // Main Method
 
     @Nullable
     @Override
