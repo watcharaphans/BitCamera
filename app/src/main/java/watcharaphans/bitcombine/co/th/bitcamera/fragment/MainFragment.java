b9aa0ebdd21ca42b33d256fef4e474da3ce0647e
watcharaphans <watcharaphans@gmail.com>
2018-08-17 13:48:00 +0700

    QR OK Thread

--- app/src/main/java/watcharaphans/bitcombine/co/th/bitcamera/fragment/MainFragment.java
+++ app/src/main/java/watcharaphans/bitcombine/co/th/bitcamera/fragment/MainFragment.java
@@ -1,35 +1,72 @@
 package watcharaphans.bitcombine.co.th.bitcamera.fragment;
 
+
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.zxing.Result;
+
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
-import watcharaphans.bitcombine.co.th.bitcamera.R;
 
-public class MainFragment extends Fragment{
+public class MainFragment extends Fragment implements ZXingScannerView.ResultHandler{
 
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-
-//        Create QRscanner
-        ZXingScannerView zXingScannerView = getView().findViewById(R.id.zxingMain);
-        zXingScannerView.startCamera();
-
-
-    } // Main Method
+    private ZXingScannerView zXingScannerView;
+    private String resultString;
+    private String tag = "17AugV1";
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater,
-                             @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_main, container, false);
-        return view;
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        zXingScannerView = new ZXingScannerView(getActivity());
+
+        return zXingScannerView;
+    }
+
+    /**
+     * Called when the fragment is visible to the user and actively running.
+     * This is generally
+     * tied to {@link Activity#onResume() Activity.onResume} of the containing
+     * Activity's lifecycle.
+     */
+    @Override
+    public void onResume() {
+        super.onResume();
+        zXingScannerView.setResultHandler(this);
+        zXingScannerView.startCamera();
+    }
+
+    /**
+     * Called when the Fragment is no longer resumed.  This is generally
+     * tied to {@link Activity#onPause() Activity.onPause} of the containing
+     * Activity's lifecycle.
+     */
+    @Override
+    public void onPause() {
+        super.onPause();
+        zXingScannerView.stopCamera();
+    }
+
+
+
+    @Override
+    public void handleResult(Result result) {
+
+        resultString = result.getText().toString().trim();
+        Log.d(tag, "Result 200ms ---> " + resultString);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                zXingScannerView.resumeCameraPreview(MainFragment.this);
+            }
+        }, 2000);
+
     }
 } //Main Class
