3a287b31db64f0f5e8408346b26864926032468d
watcharaphans <watcharaphans@gmail.com>
2018-08-31 08:41:22 +0700

    Start 31 Aug v1

--- app/src/main/java/watcharaphans/bitcombine/co/th/bitcamera/fragment/TakePhotoFragment.java
+++ app/src/main/java/watcharaphans/bitcombine/co/th/bitcamera/fragment/TakePhotoFragment.java
@@ -3,6 +3,7 @@ package watcharaphans.bitcombine.co.th.bitcamera.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Html;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
